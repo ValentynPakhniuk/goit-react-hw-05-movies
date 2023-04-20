c18fa83d260334f98a1aa66ec954c0289287dda2
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-04-20 16:44:26 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { StrictMode } from 'react';
+import ReactDOM, { createRoot } from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
