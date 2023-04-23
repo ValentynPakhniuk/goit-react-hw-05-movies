42d765314e3b58fc703a40c155cc4a5bfcb36f4d
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-04-23 21:32:45 +0300

    create Home.styled.jsx

---
+++ src/pages/Home/Home.styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const TitleTrending = styled.h1`
+  margin-left: 20px;
+  margin-bottom: 10px;
+`;
+export const CardList = styled.ul`
+  display: flex;
+  gap: 5px;
+  flex-direction: column;
+`;
