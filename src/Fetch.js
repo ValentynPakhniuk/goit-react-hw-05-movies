cd141f2a2d7fc3deb9bbe44529b6b0a00dc42c23
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-04-23 21:28:27 +0300

    update Fetch.js

--- src/Fetch.js
+++ src/Fetch.js
@@ -3,10 +3,9 @@ import axios from 'axios';
 const BASE_URL = 'https://api.themoviedb.org/3/trending/all/day';
 const API_KEY = 'ff752eca5c4aa6fb3d3aed4452b80b84';
 
-export async function getTrendingFilms() {
+export async function getTrendingFilms(controller) {
   const URL = `${BASE_URL}?api_key=${API_KEY}`;
-  const response = await axios.get(URL);
-
+  const response = await axios.get(URL, { signal: controller.signal });
   return response.data.results;
 }
 
@@ -20,23 +19,15 @@ export async function getIdFilm(movie_id) {
   const data = await axios.get(URL);
   return data.data;
 }
+
 export async function getIdFilmCredits(movie_id) {
   const URL = `https://api.themoviedb.org/3/movie/${movie_id}/credits?api_key=${API_KEY}&language=en-US`;
   const data = await axios.get(URL);
   return data.data;
 }
-// getIdFilmCredits('640146');
+
 export async function getIdFilmReviews(movie_id) {
   const URL = `https://api.themoviedb.org/3/movie/${movie_id}/reviews?api_key=${API_KEY}&language=en-US&page=1`;
   const data = await axios.get(URL);
   return data.data;
 }
-// getIdFilmReviews('640146');
-//https://api.themoviedb.org/3/movie/{movie_id}?api_key=<<api_key>>&language=en-US
-//https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key=<<api_key>>&language=en-US
-// getIdFilm('948713');
-// https://api.themoviedb.org/3/movie/550?api_key=ff752eca5c4aa6fb3d3aed4452b80b84
-// https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&page=1&include_adult=false
-// getSearchFilm('superman');
-
-// https://api.themoviedb.org/3/movie/{movie_id}/reviews?api_key=<<api_key>>&language=en-US&page=1
