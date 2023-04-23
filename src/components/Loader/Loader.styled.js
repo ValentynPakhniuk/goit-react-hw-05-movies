2c807ea543cb2bb2fe461db0fee7a4a0b7436eb2
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-04-23 21:30:08 +0300

    create Loader.styled.js

---
+++ src/components/Loader/Loader.styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import PulseLoader from 'react-spinners/PulseLoader';
+
+export const BoxLoader = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const StyledPulseLoader = styled(PulseLoader)`
+  height: 600px;
+  padding-bottom: 1000px;
+`;
