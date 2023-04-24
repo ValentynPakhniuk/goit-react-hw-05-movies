9143a482906bee38d9defa43fab0dec7e3af6a71
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-04-25 00:10:05 +0300

    update ItemFilm.jsx

--- src/components/ItemFilm/ItemFilm.jsx
+++ src/components/ItemFilm/ItemFilm.jsx
@@ -2,13 +2,13 @@ import { Link, useLocation } from 'react-router-dom';
 import { Item } from './ItemFilm.styled';
 import PropTypes from 'prop-types';
 
-export const ItemFilm = ({ film }) => {
+export const ItemFilm = ({ film: { id, title, name } }) => {
   const location = useLocation();
   return (
     <>
       <Item>
-        <Link to={`/movies/${film.id}`} state={{ from: location }}>
-          {film.title || film.name}
+        <Link to={`/movies/${id}`} state={{ from: location }}>
+          {title || name}
         </Link>
       </Item>
     </>
